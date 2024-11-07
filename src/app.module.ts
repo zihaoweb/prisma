e533be970307f4e619b9e484c7e33233c2a241a2
赵梓豪 <106091890+zihaoweb@users.noreply.github.com>
2024-11-07 16:46:31 +0800

    user and posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, PostsModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
